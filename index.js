767d1b2d784b4c41ae0246305f596dd1f11af25c
Makashi <hg52254@gmail.com>
2022-06-08 15:10:25 -0600

    Add new bot difficulty, not  finished yet

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const prompt = require('prompt-sync')();
 
 
 
+function print(message) {
+    console.log(message);
+}
 
 
 // Functions
@@ -46,53 +49,153 @@ function getUserInput() {
 
 }
 
-function getBotInput() {
-    let row = Math.floor(Math.random() * 3);
-    while (row < 0 || row > 2) {
-        row = Math.floor(Math.random() * 3);
-    }
-    let col = Math.floor(Math.random() * 3);
-    while (col < 0 || col > 2) {
-        col = Math.floor(Math.random() * 3);
-    }
-    while (!isValid(row, col)) {
-        row = Math.floor(Math.random() * 3);
-        while (row < 0 || row > 2) {
+function getBotInput(difficulty, round) {
+    print("Bot's turn");
+    if (difficulty == 1) {
+        print("Difficulty: Easy");
+        let row = Math.floor(Math.random() * 3);
+        let col = Math.floor(Math.random() * 3);
+        while (!isValid(row, col)) {
             row = Math.floor(Math.random() * 3);
-        }
-        col = Math.floor(Math.random() * 3);
-        while (col < 0 || col > 2) {
             col = Math.floor(Math.random() * 3);
         }
+        return [row, col];
+    } else if (difficulty == 2) {
+        print("Difficulty: Hard");
+        if (round == 1) {
+            print("ROUND 1!")
+            if (board[0][0] == ' ') {
+                return [0, 0];
+            } else if (board[0][2] == ' ') {
+                return [0, 2];
+            } else if (board[2][0] == ' ') {
+                return [2, 0];
+            } else if (board[2][2] == ' ') {
+                return [2, 2];
+            }
+            print("alive1")
+        } else if (round == 2) {
+            if (board[0][0] == 'O') {
+                if (isValid(0, 2)) {
+                    return [0, 2];
+                } else if (isValid(2, 0)) {
+                    return [2, 0];
+                }
+            } else if (board[0][2] == 'O') {
+                if (isValid(0, 0)) {
+                    return [0, 0];
+                } else if (isValid(2, 2)) {
+                    return [2, 2];
+                }
+            } else if (board[2][0] == 'O') {
+                if (isValid(2, 2)) {
+                    return [2, 2];
+                } else if (isValid(0, 0)) {
+                    return [0, 0];
+                }
+            } else if (board[2][2] == 'O') {
+                if (isValid(2, 0)) {
+                    return [2, 0];
+                } else if (isValid(0, 2)) {
+                    return [0, 2];
+                }
+            }
+
+        } else if (round == 3) {
+            if (board[0][0] == "O" && board[0][2] == "O") {
+                if (isValid(2, 0)) {
+                    return [2, 0];
+                } else if (isValid(2, 2)) {
+                    return [2, 2];
+                }
+            } else if (board[0][0] == "O" && board[2][0] == "O") {
+                if (isValid(0, 2)) {
+                    return [0, 2];
+                } else if (isValid(2, 2)) {
+                    return [2, 2];
+                }
+            } else if (board[0][2] == "O" && board[2][2] == "O") {
+                if (isValid(0, 0)) {
+                    return [0, 0];
+                } else if (isValid(2, 0)) {
+                    return [2, 0];
+                }
+            } else if (board[2][0] == "O" && board[2][2] == "O") {
+                if (isValid(0, 0)) {
+                    return [0, 0];
+                } else if (isValid(0, 2)) {
+                    return [0, 2];
+                }
+            } else if (board[0][0] == "O" && board[2][2] == "O") {
+                if (isValid(0, 0)) {
+                    return [0, 0];
+                } else if (isValid(2, 2)) {
+                    return [2, 2];
+                }
+            } else if (board[0][2] == "O" && board[2][0] == "O") {
+                if (isValid(0, 2)) {
+                    return [0, 2];
+                } else if (isValid(2, 0)) {
+                    return [2, 0];
+                }
+            }
+        } else if (round == 4) {
+            if (board[2][0] == "O" && board[0][0] == "O" && board[0][2] == "O") {
+                if (isValid(1, 0)) {
+                    return [1, 0];
+                } else if (isValid(0, 1)) {
+                    return [0, 1];
+                }
+            } else if (board[0][0] == "O" && board[0][2] == "O" && board[2][2] == "O") {
+                if (isValid(0, 1)) {
+                    return [0, 1];
+                } else if (isValid(2, 1)) {
+                    return [2, 1];
+                }
+            } else if (board[0][2] == "O" && board[2][2] == "O" && board[2][0] == "O") {
+                if (isValid(1, 2)) {
+                    return [1, 2];
+                } else if (isValid(2, 1)) {
+                    return [2, 1];
+                }
+            } else if (board[2][2] == "0" && board[2][0] && board[0][0] == "O") {
+                if (isValid(1, 0)) {
+                    return [1, 0];
+                } else if (isValid(2, 1)) {
+                    return [2, 1];
+                }
+            }
+
+
+        }
     }
-    return [row, col];
 }
 
 function checkWinner() {
     if (board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][0] != ' ') {
         winner = board[0][0];
-        return true;
+        return true, winner;
     } else if (board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][0] != ' ') {
         winner = board[1][0];
-        return true;
+        return true, winner;
     } else if (board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][0] != ' ') {
         winner = board[2][0];
-        return true;
+        return true, winner;
     } else if (board[0][0] == board[1][0] && board[1][0] == board[2][0] && board[0][0] != ' ') {
         winner = board[0][0];
-        return true;
+        return true, winner;
     } else if (board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[0][1] != ' ') {
         winner = board[0][1];
-        return true;
+        return true, winner;
     } else if (board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[0][2] != ' ') {
         winner = board[0][2];
-        return true;
+        return true, winner;
     } else if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
         winner = board[0][0];
-        return true;
+        return true, winner;
     } else if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') {
         winner = board[0][2];
-        return true;
+        return true, winner;
     } else {
         return false;
     }
@@ -100,22 +203,26 @@ function checkWinner() {
 
 
 
-function Main() {
+function Main(player, gameOver, winner, round, difficulty, bot) {
 
     printBoard();
 
     while (!gameOver) {
         let user = getUserInput();
-        let bot = getBotInput();
-        if (user == bot) {
-            let bot = getBotInput();
-        }
+        round += 1;
+        let bot = getBotInput(difficulty, round, user);
+
 
         board[user[0]][user[1]] = "X";
+        print(bot);
         board[bot[0]][bot[1]] = "O";
         printBoard();
-        if (checkWinner()) {
+        if (checkWinner()[0]) {
+            print(checkWinner());
+            checkWinner()[0]
             gameOver = true;
+            winner = checkWinner()[1];
+            print(winner + " wins!");
             if (winner == "X") {
                 console.log("You won!");
             } else {
@@ -126,7 +233,7 @@ function Main() {
     }
     playAgain = prompt("Play again? (y/n)   ");
     if (playAgain == "y") {
-        reset();
+        init(bot, difficulty);
     } else if (playAgain == "n") {
         console.log("Goodbye!");
     }
@@ -134,14 +241,20 @@ function Main() {
 
 }
 
-function Start() {
+function init(bot = true, difficulty = 1) {
     board = [
         [' ', ' ', ' '],
         [' ', ' ', ' '],
         [' ', ' ', ' ']
     ];
-    player = 'X';
-    gameOver = false;
-    winner = null;
-    Main();
+    let player = 'X';
+    let gameOver = false;
+    let winner = null;
+    let round = 0;
+    if (bot) {
+        Main(player, gameOver, winner, round, difficulty, bot);
+    }
+
 }
+
+init(true, 2);
